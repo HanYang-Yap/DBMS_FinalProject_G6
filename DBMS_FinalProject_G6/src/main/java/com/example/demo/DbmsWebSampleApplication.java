a2ded4e09c57b767412d0e015420dd43b521cbdc
mina0829 <116851886+mina0829@users.noreply.github.com>
2024-05-13 17:52:10 +0800

    add gradle

---
+++ DBMS_FinalProject_G6/src/main/java/com/example/demo/DbmsWebSampleApplication.java
@@ -0,0 +1,15 @@
+package main.java.com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@SpringBootApplication
+@EnableWebSecurity
+public class DbmsWebSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbmsWebSampleApplication.class, args);
+	}
+
+}
